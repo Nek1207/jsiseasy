86bf07518eeb393aa9671906d45cc2750bccaf86
Nek1207 <Nek1207@yandex.ru>
2025-09-14 18:54:31 +0500

    build202509142

--- js/script.js
+++ js/script.js
@@ -17,3 +17,31 @@
 // }
 
 // smoothScroll();
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.smoothscrolling');
+    function checkSections() {
+        sections.forEach(section => {
+            // Проверяем видимую область элемента относительно окна просмотра
+            if (isInViewport(section)) {
+                section.classList.remove('hidden');
+                section.classList.add('visible');
+            }
+        });
+    }
+
+    function isInViewport(element) {
+        let rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    window.addEventListener('scroll', checkSections);
+    checkSections();
+});
