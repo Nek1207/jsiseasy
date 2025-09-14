bcf8043121fc6df8cd08c795079f70216a2b1f2f
Nek1207 <Nek1207@yandex.ru>
2025-09-14 20:20:30 +0500

    build202509145

--- js/script.js
+++ js/script.js
@@ -37,8 +37,8 @@ document.addEventListener('DOMContentLoaded', () => {
         return (
             rect.top >= 0 &&
             rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight - 200) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth - 200)
         );
     }
 
